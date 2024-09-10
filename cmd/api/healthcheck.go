e55c75dcb8eb2eacc4180d840d4faafc86bff06d
AlanLo <alanlo@AlanLodeMacBook-Air.local>
2024-09-10 15:05:10 +0800

    3.2 JSON Ecoding

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,12 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintln(w, "environment: ", app.config.env)
-	fmt.Fprintln(w, "version: ", version)
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
+
+	js = append(js, '\n')
+
+	w.Header().Set("Content-Type", "application/json")
+
+	w.Write(js)
 }
