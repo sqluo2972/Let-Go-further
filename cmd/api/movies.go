421516e2b7fa0d8cea44cb564e55a7e45b4b161f
AlanLo <alanlo@AlanLodeMacBook-Air.local>
2024-09-10 17:39:22 +0800

    3.5 advanced JOSN Customized

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"greenlight/alan.com/internal/data"
 	"net/http"
+	"time"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,6 +17,22 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	if err != nil {
 		http.NotFound(w, r)
 		return
+	}
+
+	movie := data.Movie{
+		ID:        1,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Year:      1942,
+		Runtime:   102,
+		Genres:    []string{"Drama", "Romance", "War"},
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
 	}
 
 	fmt.Fprintf(w, "show the details of movie %d\n", id)
